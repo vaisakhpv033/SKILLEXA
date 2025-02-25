d802f7e5f0a978e0189df2983b2b6902ebc64670
Vaisakh P V <vaisakhpv033@gmail.com>
2025-02-26 00:20:42 +0530

    Implemented Sign in with google

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-from .views import CustomTokenObtainPairView, UserProfileView, RegisterUserView, AccountVerifyOTPView
+from .views import CustomTokenObtainPairView, UserProfileView, RegisterUserView, AccountVerifyOTPView, GoogleLoginView
 from django.urls import path
 from rest_framework_simplejwt.views import  TokenRefreshView
 
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
+    path("google-login/", GoogleLoginView.as_view(), name="google-login"),
     path('register/', RegisterUserView.as_view(), name='register'),
     path('register/verify/', AccountVerifyOTPView.as_view(), name='verify_otp'),
     path('profile/', UserProfileView.as_view(), name='profile'),
