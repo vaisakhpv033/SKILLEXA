b84ee9172aee55f19e07ce5c2e70329aa0345522
Vaisakh P V <vaisakhpv033@gmail.com>
2025-04-10 16:39:18 +0530

    endpoint for wishlist

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 
 from courses.models import Course
+from students.models import Enrollments
 
-from .models import Cart
+from .models import Cart, Wishlist
 
 
 class CartSerializer(serializers.ModelSerializer):
@@ -46,8 +47,69 @@ class CartSerializer(serializers.ModelSerializer):
 
         student = self.context["request"].user
 
+        # check if the course is already enrolled
+        if Enrollments.objects.filter(
+            student=student, course=value
+        ).exists():
+            raise serializers.ValidationError("You are already enrolled in this course.")
+    
         # check if the course is already in the cart
         if Cart.objects.filter(student=student, course=value).exists():
             raise serializers.ValidationError("This course is already in your cart.")
+
+        return value
+
+
+
+
+
+class WishlistSerializer(serializers.ModelSerializer):
+    """
+    Serializer for Wishlist model.
+    Provides course details within the Wishlist
+    """
+
+    course_title = serializers.CharField(source="course.title", read_only=True)
+    course_subtitle = serializers.CharField(source="course.subtitle", read_only=True)
+    course_thumbnail = serializers.URLField(source="course.thumbnail", read_only=True)
+    course_price = serializers.DecimalField(
+        source="course.price", max_digits=10, decimal_places=2, read_only=True
+    )
+    course_level = serializers.CharField(source="course.get_level_display", read_only=True)
+    topic_name = serializers.CharField(source="course.topic.name", read_only=True)
+
+    class Meta:
+        model = Wishlist
+        fields = [
+            "id",
+            "course",
+            "course_title",
+            "course_price",
+            "course_thumbnail",
+            "course_subtitle",
+            "created_at",
+            "updated_at",
+            "course_level",
+            "topic_name",
+        ]
+
+    def validate_course(self, value):
+        """
+        Ensure a course exist and is not already in the Wishlist
+        """
+
+        # check if the course exists and is published
+        if not Course.objects.filter(
+            id=value.id, status=Course.CourseStatus.PUBLISHED
+        ).exists():
+            raise serializers.ValidationError(
+                "This course is not available for purchase."
+            )
+
+        student = self.context["request"].user
+
+        # check if the course is already in the cart
+        if Wishlist.objects.filter(student=student, course=value).exists():
+            raise serializers.ValidationError("This course is already in your wishlist.")
 
         return value
--- cart/urls.py
+++ cart/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import CartViewSet
+from .views import CartViewSet, WishlistViewSet
 
 router = DefaultRouter()
 router.register(r"cart", CartViewSet, basename="cart")
+router.register(r"wishlist", WishlistViewSet, basename="wishlist")
 
 urlpatterns = [
     path("", include(router.urls)),
